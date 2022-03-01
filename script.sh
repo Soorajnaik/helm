39ddbcfdae1ed5843164eeef584f353869fad570
Ubuntu <ubuntu@ip-172-31-55-87.us-west-2.compute.internal>
2022-03-01 14:15:10 +0000

    jugaad

--- script.sh
+++ script.sh
@@ -7,4 +7,4 @@ docker tag sooraj 883195043912.dkr.ecr.us-west-2.amazonaws.com/sooraj1:sooraj
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 883195043912.dkr.ecr.us-west-2.amazonaws.com
 docker push 883195043912.dkr.ecr.us-west-2.amazonaws.com/sooraj1:sooraj
 cd ..
-helm install sooraj-clientapp client-app --namespace sooraj
+helm upgrade --install sooraj-clientapp client-app --namespace sooraj
